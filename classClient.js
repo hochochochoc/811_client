aa1f6828df8f1da87804442d98602bbe64f4dd13
Nhin_Thay <jannik.riegel@gmail.com>
2024-03-20 11:47:01 +0100

    Initial commit

---
+++ classClient.js
@@ -0,0 +1,9 @@
+class Client {
+
+    constructor(DNI, edad, nacionalidad, numVuelos){
+        this.DNI = DNI;
+        this.edad = edad;
+        this.nacionalidad = nacionalidad;
+        this.numVuelos = numVuelos;
+    }
+}
---
+++ clientes.js
@@ -0,0 +1,5 @@
+const clientes = [
+    new Client("65004204V", 30, "Español", 5),
+    new Client("74237604K", 25, "Español", 3)
+];
+let posicion = 0;
